f5e30a5f5ae252d4b5dc25864dd92300806d95a2
Heliomar P. Marques <heliomarpm@gmail.com>
2025-10-19 15:14:03 -0300

    test minimal coverage

--- index.test.js
+++ index.test.js
@@ -1,16 +1,16 @@
 const { sum, multiply } = require('./index');
 
 describe('Math Operations', () => {
-  it('sum() should correctly add two numbers', () => {
+  test('sum() should correctly add two numbers', () => {
     expect(sum(1, 2)).toBe(3);
   });
 
-  it('sum() should correctly add two negative numbers', () => {
-    expect(sum(-1, -2)).toBe(-3);
-  });
+  // test('sum() should correctly add two negative numbers', () => {
+  //   expect(sum(-1, -2)).toBe(-3);
+  // });
 
-  test('multiply() should correctly multiply two numbers', () => {
-    expect(multiply(3, 4)).toBe(12);
-  });
+  // test('multiply() should correctly multiply two numbers', () => {
+  //   expect(multiply(3, 4)).toBe(12);
+  // });
   
 });
