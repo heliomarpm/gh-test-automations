5a56b3092da89d1466db2124ab00b90b9b25191d
Heliomar P. Marques <heliomarpm@gmail.com>
2025-10-24 22:38:50 -0300

    feat: ✨ setup vitest and basic tests

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Template inicial para construção de bibliotecas em TypeScript
+ *
+ * @author Heliomar P. Marques
+ */
+import { capitalize, reverseString } from "./utils";
+
+/**
+ * Classe principal da biblioteca, pode ser utilizada para instanciar objetos
+ *
+ * @default
+ */
+export default {
+	capitalize,
+	reverseString,
+};
---
+++ test/index.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from "vitest";
+
+import lib from "../src/index";
+
+describe("Biblioteca Principal", () => {
+	it("capitalize() deve capitalizar palavras", () => {
+		expect(lib.capitalize("hello world")).toBe("Hello World");
+	});
+
+	it("reverseString() deve inverter strings", () => {
+		expect(lib.reverseString("123")).toBe("321");
+	});
+});
